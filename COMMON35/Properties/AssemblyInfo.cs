2430314fde8ab853b6cf72260c7af732704ee664
pmespace <philippemp31@outlook.com>
2020-10-02 11:34:19 +0200

    Added WMI35

--- COMMON35/Properties/AssemblyInfo.cs
+++ COMMON35/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2020.276.0727.0")]
-[assembly: AssemblyFileVersion("2020.276.0727.0")]
+[assembly: AssemblyVersion("2020.276.0928.0")]
+[assembly: AssemblyFileVersion("2020.276.0928.0")]
